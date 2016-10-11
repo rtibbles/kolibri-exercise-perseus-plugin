ab8094a5a4ea3683923945b09d3af1860502700d
66eli77 <66eli77@gmail.com>
2016-10-11 00:41:03 -0700

    switch to prop-based instead of event-based communication between parent and children components.

--- exercise_perseus_render/assets/src/vue/index.vue
+++ exercise_perseus_render/assets/src/vue/index.vue
@@ -6,6 +6,7 @@
       v-ref:wrapper
       v-if="exercise"
       :item-id="itemId"
+      :assessment-data-loaded="assessmentDataLoaded"
       :mastery-model="exercise.mastery_model"
       :mastery-spacing-time="exercise.masterySpacingTime">
       <perseus v-if="item" :item="item" v-on:nextquestion="nextQuestion"></perseus>
@@ -23,25 +24,23 @@
       item: undefined,
       exercise: undefined,
       itemId: undefined,
+      assessmentDataLoaded: false,
     }),
     methods: {
       nextQuestion() {
         const items = this.exercise.all_assessment_items;
         // still need to decide how we want to cycle through the questions
-        this.itemId = items[1];
         this.setItemData();
       },
-      setItemId() {
+      setItemData() {
         const items = this.exercise.all_assessment_items;
         const attempts = this.$refs.totalattempts || 0;
         this.itemId = items[attempts % items.length];
-        this.$emit('itemIdSet');
-      },
-      setItemData() {
         this.Kolibri.client(
           `${this.defaultFile.storage_url}${this.itemId}.json`
           ).then((itemResponse) => {
             this.item = itemResponse.entity;
+            this.assessmentDataLoaded = true;
           }).catch(function(reason) {
             console.log('Oops, you got rejected: ', reason);
           });
@@ -55,9 +54,7 @@
       this.Kolibri.client(`${this.defaultFile.storage_url}exercise.json`).then(
         (exerciseResponse) => {
           this.exercise = exerciseResponse.entity;
-          this.setItemId();
           this.setItemData();
-          this.$emit('assessmentDataLoaded');
         }).catch(function(reason) {
           console.log('Oops, requesting exercise.json got rejected: ', reason);
         });;
