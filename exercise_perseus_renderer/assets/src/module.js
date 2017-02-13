19427e2ee41b2b23874034ef904a4019049f853e
Richard Tibbles <richard.tibbles@gmail.com>
2017-02-13 11:19:22 -0800

    Revert content renderer module reference change.

--- exercise_perseus_renderer/assets/src/module.js
+++ exercise_perseus_renderer/assets/src/module.js
@@ -1,5 +1,5 @@
 
-const ContentRendererModule = require('kolibri.coreModules.contentRenderer');
+const ContentRendererModule = require('content_renderer_module');
 const ExerciseComponent = require('./vue/index');
 // Add window.btoa polyfill
 global.btoa = global.btoa ? global.btoa : require('btoa');
