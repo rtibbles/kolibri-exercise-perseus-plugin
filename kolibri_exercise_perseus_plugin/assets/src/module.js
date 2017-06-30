285aa93a546c876aeb7eed6b5f892c2003453d68
Richard Tibbles <richard.tibbles@gmail.com>
2017-06-30 13:58:20 -0700

    0.6.13 Minimal compatibility fix for perseus renderer with import syntax.

--- kolibri_exercise_perseus_plugin/assets/src/module.js
+++ kolibri_exercise_perseus_plugin/assets/src/module.js
@@ -1,5 +1,5 @@
+import ContentRendererModule from 'content_renderer_module'; // eslint-disable-line import/extensions
 
-const ContentRendererModule = require('content_renderer_module');
 const ExerciseComponent = require('./views/index');
 // Add window.btoa polyfill
 global.btoa = global.btoa ? global.btoa : require('btoa');
@@ -10,4 +10,6 @@ class ExercisePerseusModule extends ContentRendererModule {
   }
 }
 
-module.exports = new ExercisePerseusModule();
+const exercisePerseusModule = new ExercisePerseusModule();
+
+export default exercisePerseusModule;
