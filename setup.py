d797c01839289faed0a4fa9b0c1e13dabf3fd24e
Richard Tibbles <richard.tibbles@gmail.com>
2017-09-28 12:05:03 -0700

    0.7.1

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def enable_log_to_stdout(logname):
 
 setup(
     name=dist_name,
-    version="0.7.0",
+    version="0.7.1",
     description=description,
     long_description="{readme}".format(
         readme=readme,
