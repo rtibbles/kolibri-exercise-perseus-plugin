b00859faf5545562ca7bfe0d721b35967d8840a4
Richard Tibbles <richard.tibbles@gmail.com>
2017-10-06 18:23:55 -0700

    Fix incorrect Mathjax config file.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def enable_log_to_stdout(logname):
 
 setup(
     name=dist_name,
-    version="0.6.17",
+    version="0.6.18",
     description=description,
     long_description="{readme}".format(
         readme=readme,
