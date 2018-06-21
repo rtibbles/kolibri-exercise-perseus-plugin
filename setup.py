4c1521789547ffa0732d58ebf7e44a877a5963f2
indirectlylit <devon@indirectlylit.com>
2018-06-20 18:03:00 -0700

    bump develop branch to 1.2.0

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def enable_log_to_stdout(logname):
 
 setup(
     name=dist_name,
-    version="1.0.1",
+    version="1.1.0",
     description=description,
     long_description="{readme}".format(
         readme=readme,
