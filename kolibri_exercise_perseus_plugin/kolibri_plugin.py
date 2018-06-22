ae6ccf6993a64e440526ede08a81a6030d0860ab
Richard Tibbles <richard@learningequality.org>
2018-06-22 10:34:46 -0700

    Update kolibri core API reference. Bump to 1.1.0a1 for prerelease.

--- kolibri_exercise_perseus_plugin/kolibri_plugin.py
+++ kolibri_exercise_perseus_plugin/kolibri_plugin.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
-from kolibri.content import hooks as content_hooks
+from kolibri.core.content import hooks as content_hooks
 from kolibri.plugins.base import KolibriPluginBase
 
 
