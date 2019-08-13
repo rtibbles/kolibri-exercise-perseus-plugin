888de8e4d9b1235c17c5eebf90a9c5f7c1bdd594
Richard Tibbles <richard@learningequality.org>
2019-08-12 23:15:53 -0700

    Update build for updates in kolibri building.

--- kolibri_exercise_perseus_plugin/kolibri_plugin.py
+++ kolibri_exercise_perseus_plugin/kolibri_plugin.py
@@ -3,15 +3,11 @@ from __future__ import absolute_import, print_function, unicode_literals
 from kolibri.core.content import hooks as content_hooks
 from kolibri.plugins.base import KolibriPluginBase
 
-import kolibri_exercise_perseus_plugin
-
 
 class ExercisePerseusRenderPlugin(KolibriPluginBase):
     pass
 
 
 class ExercisePerseusRenderAsset(content_hooks.ContentRendererHook):
-    unique_slug = "exercise_perseus_render_module"
-    src_file = "assets/src/module.js"
+    bundle_id = "exercise_perseus_render_module"
     content_types_file = "content_types.json"
-    version = kolibri_exercise_perseus_plugin.__version__
